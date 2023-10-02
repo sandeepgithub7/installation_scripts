aad8a312aa01087ad8cc6478a791c8b4fa509505
Sandeep <141716378+sandeepgithub7@users.noreply.github.com>
2023-10-03 00:04:24 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -9,3 +9,36 @@ sudo apt install tomcat9 tomcat9-admin -y
 
 ## <user= username="tomcat" password="tomcat" roles="manager-gui,admin-gui,manager-script"/>
 
+
+
+
+##################----INSTALL TOMCAT----##################
+cd /opt
+sudo wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz
+sudo tar -xvf apache-tomcat-9.0.65.tar.gz
+
+cd /opt/apache-tomcat-9.0.65/conf
+sudo vi tomcat-users.xml
+# ---add-below-line at the end (2nd-last line)----
+# <user username="admin" password="admin1234" roles="admin-gui, manager-gui"/>
+
+sudo ln -s /opt/apache-tomcat-9.0.65/bin/startup.sh /usr/bin/startTomcat
+sudo ln -s /opt/apache-tomcat-9.0.65/bin/shutdown.sh /usr/bin/stopTomcat
+
+sudo vi /opt/apache-tomcat-9.0.65/webapps/manager/META-INF/context.xml
+
+comment:
+<!-- Valve className="org.apache.catalina.valves.RemoteAddrValve"
+  allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
+
+sudo vi /opt/apache-tomcat-9.0.65/webapps/host-manager/META-INF/context.xml
+
+comment:
+<!-- Valve className="org.apache.catalina.valves.RemoteAddrValve"
+  allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
+
+sudo stopTomcat
+sudo startTomcat
+
+sudo cp target/*.war /opt/apache-tomcat-9.0.65/webapps/
+
