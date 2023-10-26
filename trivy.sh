fc864a1f078b35408969a94ea28b7e1727db713b
Sandeep <141716378+sandeepgithub7@users.noreply.github.com>
2023-10-26 09:08:45 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -5,3 +5,9 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-k
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
 sudo apt-get install trivy
+**********************************************************************************************************************************
+
+wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.deb
+sudo dpkg -i trivy_0.18.3_Linux-64bit.deb
+
+**********************************************************************************************************************************
