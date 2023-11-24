4e702f5c1b3f6e3bff788434ebdb43f0ce84f52b
Sandeep <141716378+sandeepgithub7@users.noreply.github.com>
2023-11-24 16:42:07 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -13,3 +13,18 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y 
 sudo apt-get install jenkins -y
+
+************** To Change Jenkins Port ****************
+
+sudo systemctl stop jenkins
+sudo systemctl status jenkins
+cd /etc/default
+sudo vi jenkins   #chnage port HTTP_PORT=8090 and save and exit
+cd /lib/systemd/system
+sudo vi jenkins.service  #change Environments="Jenkins_port=8090" save and exit
+sudo systemctl daemon-reload
+sudo systemctl restart jenkins
+sudo systemctl status jenkins
+
+*******************************************************
+
